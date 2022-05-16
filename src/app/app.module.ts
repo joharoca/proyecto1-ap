7abb819390b81c164e2b5be0b0a0e152e2081b5c
Johana Roca <roca.johanam@gmail.com>
2022-05-15 22:35:49 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FirstTryComponent } from './components/first-try/first-try.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstTryComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/first-try/first-try.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-try',
+  templateUrl: './first-try.component.html',
+  styleUrls: ['./first-try.component.css']
+})
+export class FirstTryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
